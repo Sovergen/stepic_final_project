d20b75b0256736dd2bf7d344b9d47fc35103f61f
sovergen <flying-bear@ya.ru>
2023-03-17 14:24:03 +0400

    add test for login page

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,5 @@
-from selenium.webdriver.common.by import By
+from .main_page import MainPage
+from paget
 
 def go_to_login_page(browser):
     login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
@@ -8,3 +9,10 @@ def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     browser.get(link)
     go_to_login_page(browser)
+
+    
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+    page.open()                      # открываем страницу
+    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
