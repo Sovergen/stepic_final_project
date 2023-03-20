379ca06919d563bb449361103f76f7a42f7ea444
sovergen <flying-bear@ya.ru>
2023-03-21 00:07:10 +0400

    clean unused markers

--- test_product_page.py
+++ test_product_page.py
@@ -4,7 +4,6 @@ from .pages.product_page import ProductPage
 from .pages.basket_page import BasketPage
 from .pages.login_page import LoginPage
 
-@pytest.mark.basket_actions
 class TestUserAddToBasketFromProductPage():
 	@pytest.fixture(scope="function", autouse=True)
 	def setup(self,browser):
@@ -16,7 +15,7 @@ class TestUserAddToBasketFromProductPage():
 		register_page.register_new_user(email=email , password=password)
 		register_page.should_be_authorized_user()
 
-
+	@pytest.mark.need_review
 	def test_user_cant_see_success_message(self, browser):
 		link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 		product_page = ProductPage(browser, link)
@@ -41,18 +40,27 @@ class TestUserAddToBasketFromProductPage():
 		product_page.should_be_add_rigt_product()
 		product_page.should_be_right_basket_price()
 
+@pytest.mark.need_review
+def test_guest_cant_see_success_message(browser):
+		link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+		product_page = ProductPage(browser, link)
+		product_page.open()
+		product_page.should_not_be_success_message_present()
+
 def test_guest_should_see_login_link_on_product_page(browser):
 	link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
 	page = ProductPage(browser, link)
 	page.open()
 	page.should_be_login_link()
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
 	link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
 	page = ProductPage(browser, link)
 	page.open()
 	page.go_to_login_page()
 
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
 	link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 	product_page = ProductPage(browser, link)
