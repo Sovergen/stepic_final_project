e87983c94c7ba892dc7ad9d4c464d09a8650ad5f
sovergen <flying-bear@ya.ru>
2023-03-20 18:05:42 +0400

    added test for product page

---
+++ test_product_page.py
@@ -0,0 +1,12 @@
+from .pages.product_page import ProductPage
+import time
+
+def test_guest_can_add_product_to_basket(browser):
+	link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+	product_page = ProductPage(browser, link)
+	product_page.open()
+	product_page.add_product_to_basket()
+	product_page.solve_quiz_and_get_code()
+	product_page.should_be_add_rigt_product()
+	product_page.should_be_right_basket_price()
+
