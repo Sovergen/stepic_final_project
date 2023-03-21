d15f6f6762724535b2e352183ff63b3ff182f142
sovergen <flying-bear@ya.ru>
2023-03-21 11:19:07 +0400

    remove unused imports and cut parameters quantity

--- test_product_page.py
+++ test_product_page.py
@@ -22,16 +22,10 @@ class TestUserAddToBasketFromProductPage():
 		product_page.open()
 		product_page.should_not_be_success_message_present()
 
-	@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
-	                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
-	                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
-	                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
-	                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
-	                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
-	                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+	#I am cut parameters quantity for esieer check
+	@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
 	                                  pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
-	                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
-	                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+	                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8"])
 	def test_user_can_add_product_to_basket(self, browser, link):
 		product_page = ProductPage(browser, link)
 		product_page.open()
